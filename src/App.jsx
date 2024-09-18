adf95b7d07b73938dfa9c2f2d050259256ac1c38
unknown <abijo356@gmail.com>
2024-09-18 14:17:02 +0100

    fourth commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,7 @@ import Banner2 from './components/Banner/Banner2';
 import Testimonials from './components/Testimonials';
 import Newsletter from './components/Newsletter/Newsletter';
 import Footer from './components/Footer/Footer';
-// import HeroSection from './components/HeroSection/Hero';
-// import Menus from './components/Menus/Menus';
-// import Banner from './components/Banner/Banner';
-// import Banner2 from './components/Banner/Banner2';
-// import Banner3 from './components/Banner/Banner3';
-// import Footer from './components/Footer/Footer'
-// import FeatureSection from './components/FeatureSection'
-// import Workflow from './components/WorkFlow'
-// import Pricing from './components/Pricing'
-// import Testimonials from './components/Testimonials'
-// import Footer from './components/Footer'
+
 
 const App = () => {
   return (
@@ -32,19 +22,6 @@ const App = () => {
     <Testimonials />
     <Newsletter />
     <Footer />
-    {/* <HeroSection />
-    <Menus />
-    <Banner />
-    <Banner2 />
-    <Banner3 />
-    <Footer />  */}
-    {/* <div className="max-w-7xl mx-auto pt-20 px-6">
-    <HeroSection />
-    <FeatureSection />
-    <Workflow />
-    <Pricing />
-    <Testimonials />
-    <Footer /> */}
     </div>
     
   )
